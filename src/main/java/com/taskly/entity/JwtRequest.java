867bcd6342f13385cb5cf3f0c158e1d46b9dc56f
Akshat Nigam <nigamakshat29jan2000@gmail.com>
2025-02-22 22:56:37 +0530

    Added auth controller and jwt request and response entity

---
+++ src/main/java/com/taskly/entity/JwtRequest.java
@@ -0,0 +1,14 @@
+package com.taskly.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class JwtRequest {
+    private String email;
+    private String password;
+}
